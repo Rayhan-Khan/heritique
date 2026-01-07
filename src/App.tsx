35ac6bae8d963da14017c7c83a5e97a38ddb1c33
Rayhan-Khan <rayhankhan014@gmail.com>
2026-01-07 23:57:15 +0600

    proceed checkout

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import KitchenStoragePage from './pages/KitchenStoragePage';
 import BlogPage from './pages/BlogPage';
 import ContactPage from './pages/ContactPage';
 import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
 import ProductDetailPage from './pages/ProductDetailPage';
@@ -26,6 +27,7 @@ function App() {
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/registration" element={<RegistrationPage />} />
 
