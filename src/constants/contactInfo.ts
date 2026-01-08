b4caaed5ed0ed003f00418da352176580d8b9504
Md-Rabiul-Islam75 <rabiul77777888@gmail.com>
2026-01-08 18:23:31 +0600

    update the json file with relevant kitchen item

--- src/constants/contactInfo.ts
+++ src/constants/contactInfo.ts
@@ -2,7 +2,7 @@ export const CONTACT_INFO = {
   email: 'pappow@gmail.com',
   phone: '+1 (347) 698-5455',
   phoneRaw: '01XXX', // Display format for some contexts
-  address: 'Dhaka Gulshan',
+  address: '28-07 Jackson Ave, Long Island City, NY, 11101, USA',
   owner: 'A B M Whaiduzzaman',
   ownerTitle: 'Owner'
 } as const;
