97cd5e19adedb52120dbcf1a9f26e6a0719a69dd
Md-Rabiul-Islam75 <rabiul77777888@gmail.com>
2026-01-14 13:17:53 +0600

    address and email change

--- src/constants/contactInfo.ts
+++ src/constants/contactInfo.ts
@@ -1,8 +1,8 @@
 export const CONTACT_INFO = {
-  email: 'pappow@gmail.com',
+  email: 'whaid@heritiqueintl.com',
   phone: '+1 (347) 698-5455',
   phoneRaw: '01XXX', // Display format for some contexts
-  address: '28-07 Jackson Ave, Long Island City, NY, 11101, USA',
+  address: '30 N GOULD ST 42588\nSHERIDAN WY 82801',
   owner: 'A B M Whaiduzzaman',
   ownerTitle: 'Owner'
 } as const;
